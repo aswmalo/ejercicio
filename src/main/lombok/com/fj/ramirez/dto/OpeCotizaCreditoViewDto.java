d99234f499bb1dfa8b08d85abdead63a1ff79366
framirez <framirez>
2022-01-23 18:24:31 -0600

    ksdjksd

---
+++ src/main/lombok/com/fj/ramirez/dto/OpeCotizaCreditoViewDto.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.fj.ramirez.dto;
+
+/**
+ * @author framirez21
+ *
+ */
+public class OpeCotizaCreditoViewDto extends AbstractBaseDto  {
+	private static final long serialVersionUID = 1L;
+}
